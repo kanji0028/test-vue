b43ecdc7b9b61d7e06bbf1496066636cb08a405e
大庭貫治 <k.ooniwa@so-ra.co.jp>
2023-01-15 08:47:44 +0900

    中間テーブル・ダミーデータ作成

--- app/app/Models/Purchase.php
+++ app/app/Models/Purchase.php
@@ -4,23 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Purchase;
+use App\Models\Customer;
+use App\Models\Item;
 
-class Item extends Model
+class Purchase extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-      'name',
-      'memo',
-      'price',
-      'is_selling',
+      'customer_id',
+      'status',
     ];
 
-    // 多対多の中間テーブル用リレーション
-    public function purchases()
+    public function customer()
     {
-        return $this->belongsToMany(Purchase::class)
+        return $this->belongsTo(Customer::class);
+    }
+
+    // 多対多の中間テーブル用リレーション
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)
         ->withPivot('quantity');
     }
 }
