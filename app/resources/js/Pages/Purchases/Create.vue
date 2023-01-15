2225009c659687f472c1fa167a3256f1d139e4cf
大庭貫治 <k.ooniwa@so-ra.co.jp>
2023-01-15 15:39:12 +0900

    micromodaljs 追加

--- app/resources/js/Pages/Purchases/Create.vue
+++ app/resources/js/Pages/Purchases/Create.vue
@@ -5,6 +5,7 @@ import { getToday } from "@/common";
 import { reactive, onMounted, ref, computed } from "vue";
 import { Inertia } from '@inertiajs/inertia'
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import MicroModal from '@/Components/MicroModal.vue'
 
 const props = defineProps({
   'customers': Array,
@@ -88,6 +89,7 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
                       <div class="p-2 w-full">
                         <div class="relative">
+                          <MicroModal />
                           <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
                           <select type="text" id="customer" name="customer" v-model="form.customer_id"
                             class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
